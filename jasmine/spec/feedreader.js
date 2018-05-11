fe8029ad88db06f937cd268346aabe05c72c222f
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-05-10 20:39:33 -0400

    clean up the feedreader.js page

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,12 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function() {
+$(function () {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', function () {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -50,20 +50,20 @@ $(function() {
         });
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
         var body = $('body'),
             menuIcon = $('.menu-icon-link');
 
         /* TODO: Write a test that ensures the menu element is
-        * hidden by default. You'll have to analyze the HTML and
-        * the CSS to determine how we're performing the
-        * hiding/showing of the menu element.
-        */
+    * hidden by default. You'll have to analyze the HTML and
+    * the CSS to determine how we're performing the
+    * hiding/showing of the menu element.
+    */
         it('the menu element should be hidden by default', function () {
             expect(body.hasClass('menu-hidden')).toBe(true);
         });
+
         /* TODO: Write a test that ensures the menu changes
         * visibility when the menu icon is clicked. This test
         * should have two expectations: does the menu display when
@@ -77,7 +77,7 @@ $(function() {
             //checks if menu-hidden is true when clicked the second time
             menuIcon.click();
             expect(body.hasClass('menu-hidden')).toBe(true);
-        });        
+        });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
@@ -98,7 +98,7 @@ $(function() {
             //expects that there is at least one entry
             expect(entries.length).toBeGreaterThan(0);
             done();
-        });        
+        });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function () {
@@ -121,5 +121,5 @@ $(function() {
             expect(newUrl).not.toBe(previousUrl);
             done();
         });
-    })     
+    })
 }());
